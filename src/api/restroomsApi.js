e82e0bc9743336a7e4ed5e1801e385f5832985fe
Gary Ng <garyng@garys-mbp.attlocal.net>
2025-12-27 16:36:42 -0800

    can use patch and read env var for VITE_API_BASE_URL

--- src/api/restroomsApi.js
+++ src/api/restroomsApi.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8787/api";
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export async function fetchRestroomsByBounds(bounds) {
   const url = new URL(`${API_BASE}/restrooms`);
